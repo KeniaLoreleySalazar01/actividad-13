340948d8ff4221a22c9f079622000f24cd0412f2
Keny <160526949+KeniaLoreleySalazar01@users.noreply.github.com>
2024-06-14 01:59:33 +0000

    Initial commit

---
+++ README .txt
@@ -0,0 +1,8 @@
+datos de entrada: num 1,num 2
+datos de salida: resultado de operación
+inicio
+ingresar valor 1
+ingresar valor 2
+hacer num1 / num 2
+mostrar resultado en pantalla
+fin
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Esto da el resultado de una división</p>';
+    $num = 10;
+    $num2 = 2;
+    $result = $num / $num2;
+    echo "Resultado de la división: " . $result;
+    ?> 
+
+</html>
